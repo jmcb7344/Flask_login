88711a03aace3582a7a2495c13ed7dea0b2041c1
jmcb7344 <castrojuan1990@gmail.com>
2022-08-30 22:14:46 -0300

    1

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 class Config():
     SECRET_KEY = 'clave_secreta'
     DEBUG = True
+    #Se puede realizar con Postgresql
     #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:7344@localhost/flask_login'
+    #Se puede realizar con SQLite
     SQLALCHEMY_DATABASE_URI = "sqlite:///base_datos/flask_db.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
